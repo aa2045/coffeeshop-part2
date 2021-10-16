004956293fb398a6d90914d0551f05b6b996ff58
aa2045 <72710454+aa2045@users.noreply.github.com>
2021-10-16 20:56:47 +0400

    Add files via upload

---
+++ coffeshop-stage2/vpproject/coffeshop/App.java
@@ -0,0 +1,13 @@
+package f21as.coursework.f21as.coursework.coffeshop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
